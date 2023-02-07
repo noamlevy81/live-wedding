8ce776c87f7558345dc046e5e7224401d03d125b
noamlevy <noaml@activefence.com>
2023-02-07 23:16:25 +0200

    bugfix

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
-const S3Client = require('@aws-sdk/client-s3');
+const {S3Client} = require('@aws-sdk/client-s3');
 const S3SyncClient = require('s3-sync-client');
+const fs = require('fs');
 
-const s3Client = new S3Client({ region: 'eu-west-1'});
+const s3Client = new S3Client({ region: 'eu-west-1', });
+s3Client.removeListe
 const { sync } = new S3SyncClient({ client: s3Client });
 
 const SYNC_INTERVAL = 5000;
-const BUCKET = 'or-and-noam-wedding-bucket';
-const LOCAL_PATH = '/.wedding-images';
+const BUCKET = 'or-and-noam-wedding-bucket/images';
+const LOCAL_PATH = './wedding-images';
+//Todo: check for credentials workaround
 
 (async () => {
-    const syncOps = await sync(`s3://${BUCKET}`, LOCAL_PATH);
-    console.log(syncOps);
+    while (1) {
+        const syncOps = await sync(`s3://${BUCKET}`, LOCAL_PATH);
+        console.log(syncOps);
 
-    await new Promise(r => setTimeout(r, SYNC_INTERVAL));
-
+        await new Promise(r => setTimeout(r, SYNC_INTERVAL));
+    }
 })();
